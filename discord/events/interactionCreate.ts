de9354d1683f88239b63a4d3d5b71801599dc6a3
Int Fract <intfract@gmail.com>
2023-08-26 11:37:29 +0000

    fixed type safety

--- discord/events/interactionCreate.ts
+++ discord/events/interactionCreate.ts
@@ -1,7 +1,7 @@
-import { CommandInteraction } from 'discord.js'
+import { BaseInteraction } from 'discord.js'
 import client from '..'
 import { commands } from '../commands'
 
-export function respond(interaction: CommandInteraction) {
+export function respond(interaction: BaseInteraction) {
   if (interaction.isChatInputCommand()) return commands[interaction.commandName].run(client, interaction)
 }
