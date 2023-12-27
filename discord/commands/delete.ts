176b40bf37bed8358547864bbdc5b77e72a73fd4
Int Fract <intfract@gmail.com>
2023-12-27 10:54:49 +0000

    fixed message filtering for `delete` command

--- discord/commands/delete.ts
+++ discord/commands/delete.ts
@@ -1,5 +1,5 @@
-import { ApplicationCommandOptionType, ApplicationCommandType, Client, ColorResolvable, CommandInteraction, EmbedBuilder, InteractionResponse, PermissionsBitField } from 'discord.js'
-import { getLastMessages } from '../database'
+import { ApplicationCommandOptionType, ApplicationCommandType, Client, ColorResolvable, CommandInteraction, EmbedBuilder, InteractionResponse, Message, PermissionsBitField } from 'discord.js'
+import { getLastMessages, getLastMessagesBy } from '../database'
 
 export default {
   name: 'delete',
@@ -13,6 +13,12 @@ export default {
       type: ApplicationCommandOptionType.Integer,
       required: true,
     },
+    {
+      name: 'author',
+      description: 'author of the messages',
+      type: ApplicationCommandOptionType.User,
+      required: false,
+    },
   ],
   run: async (client: Client, interaction: CommandInteraction) => {
     const { user, channelId } = interaction
@@ -21,14 +27,20 @@ export default {
     if (!author?.permissions.has(PermissionsBitField.Flags.ManageMessages)) return interaction.reply({ content: 'You do not have permission to manage messages!', ephemeral: true })
     const count = interaction.options.get('count')?.value as number
     if (count <= 0) return interaction.reply({ content: 'You must input an integer greater than 0.', ephemeral: true })
+    const messageAuthor = interaction.options.get('author')?.user
     
     await interaction.deferReply({ ephemeral: true })
-    const messages = await getLastMessages(client, channelId, count)
+    let messages: Message[]
+    if (messageAuthor) {
+      messages = await getLastMessagesBy(client, channelId, count, message => message.author.equals(messageAuthor))
+    } else {
+      messages = await getLastMessages(client, channelId, count)
+    }
     messages.forEach(message => {
       if (message.deletable) message.delete()
       else return interaction.editReply({ content: `[This](${message.url}) message is not deletable!` })
     })
 
-    return interaction.editReply({ content: `${messages.length} message${messages.length > 1 ? 's were' : ' was'} deleted!` })
+    return interaction.editReply({ content: `${messages.length} message${messages.length === 1 ? ' was' : 's were'} deleted!` })
   }
 }
--- discord/database.ts
+++ discord/database.ts
@@ -3,10 +3,10 @@ import { Client, FetchMessagesOptions, Message, TextChannel } from 'discord.js'
 const FETCH_LIMIT = 100
 
 /**
- * gets messages in a channel in chronological order
+ * gets all the messages in a channel
  * @param client the bot client
  * @param channelId ID of channel to fetch messages from
- * @returns {Promise<Message<boolean>[]>}
+ * @returns {Promise<Message<boolean>[]>} messages in chronological order
  */
 
 export async function getMessages(client: Client, channelId: string): Promise<Message<boolean>[]> {
@@ -46,10 +46,10 @@ export async function getMessagesBy(client: Client, channelId: string, callback:
 }
 
 /**
- * gets messages in a channel in reverse chronological order
+ * gets a certain number of messages last sent in the channel
  * @param client the bot client
  * @param channelId ID of channel to fetch messages from
- * @returns {Promise<Message<boolean>[]>}
+ * @returns {Promise<Message<boolean>[]>} messages in reverse chronological order
  */
 
 export async function getLastMessages(client: Client, channelId: string, count: number): Promise<Message<boolean>[]> {
@@ -58,14 +58,40 @@ export async function getLastMessages(client: Client, channelId: string, count: 
   if (!channel?.isTextBased()) return messages
 
   let pointer: Message | undefined | null
-  let options: FetchMessagesOptions = { limit: count > FETCH_LIMIT ? FETCH_LIMIT : count }
+  let options: FetchMessagesOptions
   do {
+    options = { limit: count > FETCH_LIMIT ? FETCH_LIMIT : count }
     if (pointer) options.before = pointer.id
     pointer = await channel.messages.fetch(options).then(messagePage => {
       messagePage.forEach(message => messages.push(message))
       return messagePage.size === FETCH_LIMIT ? messagePage.at(-1) : null
     })
-  } while (pointer)
+    count -= FETCH_LIMIT
+  } while (pointer && count > 0)
+  
+  return messages
+}
+
+export async function getLastMessagesBy(client: Client, channelId: string, count: number, callback: (message: Message) => boolean): Promise<Message<boolean>[]> {
+  const channel = client.channels.cache.get(channelId)
+  const messages: Message[] = []
+  if (!channel?.isTextBased()) return messages
+
+  let pointer: Message | undefined | null
+  let options: FetchMessagesOptions
+  do {
+    options = { limit: count > FETCH_LIMIT ? FETCH_LIMIT : count }
+    if (pointer) options.before = pointer.id
+    pointer = await channel.messages.fetch(options).then(messagePage => {
+      messagePage.forEach(message => {
+        if (callback(message)) {
+          messages.push(message)
+          count--
+        }
+      })
+      return messagePage.size === FETCH_LIMIT || count > 0 ? messagePage.at(-1) : null
+    })
+  } while (pointer && count > 0)
   
   return messages
 }
